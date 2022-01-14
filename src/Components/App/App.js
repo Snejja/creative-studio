0ba63f6730310ab65399242ae4e1eb8c974e4931
Snejja <may06@mail.ru>
2022-01-14 13:52:46 +0300

    Added section services

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,10 +1,12 @@
 import Main from '../Main';
 import About from '../About';
+import Services from '../Services';
 
 const App = () => (
   <div className='app'>
     <Main />
     <About />
+    <Services />
   </div>
 );
 
---
+++ src/Components/Services/Services.js
@@ -0,0 +1,68 @@
+import cn from 'classnames';
+
+import { ReactComponent as BrandingIcon } from './draft.svg';
+import { ReactComponent as DesignIcon } from './canvas.svg';
+import { ReactComponent as DevelopmentIcon } from './workspace.svg';
+
+import styles from './Services.module.css';
+
+const Services = () => (
+  <div className={styles.services}>
+    <div className={styles.content}>
+      <h1 className={styles.title}>Our Services</h1>
+      <div className={styles.gap} />
+      <div className={styles.containers}>
+        <div className={styles.container}>
+          <div className={styles.header}>
+            <div>
+              <h2 className={styles.name}>branding</h2>
+              <div className={cn(styles.gap, styles.gapSmall)} />
+            </div>
+            <div className={styles.icon}>
+              <BrandingIcon />
+            </div>
+          </div>
+          <p className={styles.text}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt.
+          </p>
+          <button className={styles.btn}>Read more</button>
+        </div>
+        <div className={styles.container}>
+          <div className={styles.header}>
+            <div>
+              <h2 className={styles.name}>design</h2>
+              <div className={cn(styles.gap, styles.gapSmall)} />
+            </div>
+            <div className={styles.icon}>
+              <DesignIcon />
+            </div>
+          </div>
+          <p className={styles.text}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt.
+          </p>
+          <button className={styles.btn}>Read more</button>
+        </div>
+        <div className={styles.container}>
+          <div className={styles.header}>
+            <div>
+              <h2 className={styles.name}>development</h2>
+              <div className={cn(styles.gap, styles.gapSmall)} />
+            </div>
+            <div className={styles.icon}>
+              <DevelopmentIcon />
+            </div>
+          </div>
+          <p className={styles.text}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt.
+          </p>
+          <button className={styles.btn}>Read more</button>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default Services;
